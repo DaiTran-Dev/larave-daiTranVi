3c4cb8ef4c3c2a6066d6df62a9bb89af792e9eb2
DaiTran-Dev <daivitran.dev@gmail.com>
2021-04-16 10:30:12 +0700

    udapte

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+//API articel
 Route::get('articels','ArticleController@getArticel');
 Route::get('articels/{articel}','ArticleController@searchArticel');
 Route::post('articels','ArticleController@creatArticel');
